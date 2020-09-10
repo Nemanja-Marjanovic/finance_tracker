22363a24485fe4d3cccc16987728ad542e3fec77
Nemanja Marjanovic <mr.nemanja.marjanovic@gmail.com>
2020-09-10 14:25:44 +0200

    Stock lookup method for stock info

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+    publishable_token: 'Tpk_2e0528073b4449638d3ab1410dea6ace',
+    endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price(ticker_symbol)
+  end
+
 end
